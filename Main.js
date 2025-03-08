a8875a4e45f92a0602188b895729a9d02de5bd30
Dgarridoo <danielgarciagarrido96@gmail.com>
2025-03-08 15:31:12 +0100

    deplo

--- Main.js
+++ Main.js
@@ -1,35 +1,35 @@
 let menuVisible = false;
+
 //Funcion que oculta o muestra el menu
 function mostrarOcultarMenu() {
-    if (menuVisible) {
-        document.getElementById("nav").classList = "";
+    if (menuVisible){
+        document.getElementById("nav").classList.remove("responsive");
         menuVisible = false;
     } else {
-        document.getElementById("nav").classList = "responsive";
+        document.getElementById("nav").classList.add("responsive");
         menuVisible = true;
     }
 }
 
 function seleccionar(){
     //oculto el menu una vez que selecciono una opcion
-    document.getElementById("nav").classList ="";
+    document.getElementById("nav").classList.remove("responsive");
     menuVisible = false;
 }
+
 //Funcion que aplica las animaciones de las habilidades
 function efectoHabilidades() {
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript")
-        habilidades[1].classList.add("bash")
-        habilidades[2].classList.add("htmlcss")
-        habilidades[3].classList.add("javascript")
-
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("bash");
+        habilidades[2].classList.add("htmlcss");
     }
 }
 
 //detecto el scroll para aplicar las animaciones
 window.onscroll = function () {
-    efectoHabilidades()
+    efectoHabilidades();
 }
